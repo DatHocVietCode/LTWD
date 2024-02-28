1dd06e6f8230dd1f33136744b88b014650f5d986
Võ Phan Tấn Đạt <125366123+DatHocVietCode@users.noreply.github.com>
2024-02-28 14:02:21 +0700

    Database HocSinh

---
+++ HocSinh.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[HocSinh] (
+    [ID]     NVARCHAR (MAX) NULL,
+    [Cmnd]   VARCHAR (100)  NULL,
+    [Ten]    VARCHAR (100)  NULL,
+    [Sex]    NCHAR (10)     NULL,
+    [DoB]    DATETIME2 (7)  NULL,
+    [Diachi] VARCHAR (255)  NULL,
+    [Email]  VARCHAR (50)   NULL,
+    [Phone]  VARCHAR (50)   NULL,
+    [Diem]   FLOAT (53)     NULL
+);
