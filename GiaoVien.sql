e6edccd0248a80f26d6869f4a1f1d536214a1e50
Võ Phan Tấn Đạt <125366123+DatHocVietCode@users.noreply.github.com>
2024-02-28 14:02:51 +0700

    Create GiaoVien.sql
    DataBase GiaoVien

---
+++ GiaoVien.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[GiaoVien] (
+    [ID]     NVARCHAR (MAX) NULL,
+    [Cmnd]   VARCHAR (100)  NULL,
+    [Ten]    VARCHAR (100)  NULL,
+    [Sex]    NCHAR (10)     NULL,
+    [DoB]    DATETIME2 (7)  NULL,
+    [Diachi] VARCHAR (255)  NULL,
+    [Email]  VARCHAR (50)   NULL,
+    [Phone]  VARCHAR (50)   NULL
+);
+
